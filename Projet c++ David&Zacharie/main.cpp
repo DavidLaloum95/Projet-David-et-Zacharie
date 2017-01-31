0e3a0395e3934f97eb32f47436a8f3407ad5269a
DavidLaloum95 <davidlaloum95@gmail.com>
2017-01-31 15:02:56 +0100

    Initial Commit

---
+++ Projet c++ David&Zacharie/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projet c++ David&Zacharie
+//
+//  Created by David Laloum on 31/01/2017.
+//  Copyright © 2017 David Laloum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
